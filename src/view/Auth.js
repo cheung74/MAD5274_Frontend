90c740d9ac850a12a9195b4786d291670c77efa5
mark chan <mark_chan74@hotmail.com>
2022-12-06 19:08:50 -0500

    ✅ add video, ads

--- src/view/Auth.js
+++ src/view/Auth.js
@@ -1,24 +1,25 @@
+import { useNavigation } from "@react-navigation/native";
+import { Video } from "expo-av";
+import React from "react";
 import {
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-  ImageBackground,
+  Dimensions,
   KeyboardAvoidingView,
   Platform,
+  SafeAreaView,
+  StyleSheet,
+  View,
 } from "react-native";
-import React from "react";
-import { Colors } from "../utils/Colors";
-import CustomTextInput from "../components/CustomTextInput";
+import { StatusBar } from "expo-status-bar";
+
 import CustomButton from "../components/CustomButton";
-import { createUser } from "../services/user";
-import { storeLocalUserData } from "../services/asyncStorage";
-import { useNavigation } from "@react-navigation/native";
+import CustomTextInput from "../components/CustomTextInput";
 import { useAuth } from "../hooks/useAuth";
+
+const { height } = Dimensions.get("window");
 
 const Auth = () => {
   const navigation = useNavigation();
+  const video = React.useRef(null);
   const {
     email,
     setEmail,
@@ -42,92 +43,100 @@ const Auth = () => {
   };
 
   return (
-    <ImageBackground
-      style={styles.image}
-      source={require("../assets/images/auth_background_1.jpg")}
-    >
-      <SafeAreaView style={{ flex: 1 }}>
-        <KeyboardAvoidingView
-          style={{ flexGrow: 1 }}
-          behavior={Platform.OS === "ios" ? "padding" : null}
-        >
-          <View style={styles.container}>
-            <View style={{ marginTop: "auto" }} />
-            {!isLogin ? (
-              <>
-                <CustomTextInput
-                  style={{ marginVertical: 8 }}
-                  value={email}
-                  onChangeText={setEmail}
-                  placeholder="Enter your email"
-                  textContentType="emailAddress"
-                />
-                <CustomTextInput
-                  style={{ marginVertical: 8 }}
-                  value={password}
-                  onChangeText={setPassword}
-                  placeholder="Enter your password"
-                  textContentType="password"
-                />
+    <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar style="light" />
+      <Video
+        shouldPlay
+        ref={video}
+        useNativeControls
+        source={require("../assets/videos/loginFootage.mp4")}
+        style={styles.backgroundVideo}
+        muted={true}
+        resizeMode={"cover"}
+        rate={1.0}
+        isLooping
+        ignoreSilentSwitch={"obey"}
+      />
+      <KeyboardAvoidingView
+        style={{ flexGrow: 1 }}
+        behavior={Platform.OS === "ios" ? "padding" : null}
+      >
+        <View style={styles.container}>
+          <View style={{ marginTop: "auto" }} />
+          {!isLogin ? (
+            <>
+              <CustomTextInput
+                style={{ marginVertical: 8 }}
+                value={email}
+                onChangeText={setEmail}
+                placeholder="Enter your email"
+                textContentType="emailAddress"
+              />
+              <CustomTextInput
+                style={{ marginVertical: 8 }}
+                value={password}
+                onChangeText={setPassword}
+                placeholder="Enter your password"
+                textContentType="password"
+              />
 
-                <CustomButton
-                  style={{ marginVertical: 8 }}
-                  title="Submit"
-                  onPress={handleLogin}
-                  loading={loading}
-                />
-                <CustomButton
-                  style={{ marginVertical: 8 }}
-                  title="Register"
-                  onPress={handleBack}
-                />
-              </>
-            ) : (
-              <>
-                <CustomTextInput
-                  style={{ marginVertical: 8 }}
-                  value={email}
-                  onChangeText={setEmail}
-                  placeholder="Enter your email"
-                  textContentType="emailAddress"
-                />
-                <CustomTextInput
-                  style={{ marginVertical: 8 }}
-                  value={fullName}
-                  onChangeText={setFullName}
-                  placeholder="Enter your full name"
-                />
-                <CustomTextInput
-                  style={{ marginVertical: 8 }}
-                  value={mobile}
-                  onChangeText={setMobile}
-                  textContentType="telephoneNumber"
-                  placeholder="Enter your phone number"
-                />
-                <CustomTextInput
-                  style={{ marginVertical: 8 }}
-                  value={password}
-                  onChangeText={setPassword}
-                  placeholder="Enter your password"
-                  textContentType="password"
-                />
-                <CustomButton
-                  style={{ marginVertical: 8 }}
-                  title="Submit"
-                  onPress={handleRegister}
-                  loading={loading}
-                />
-                <CustomButton
-                  style={{ marginVertical: 8 }}
-                  title="Back"
-                  onPress={handleBack}
-                />
-              </>
-            )}
-          </View>
-        </KeyboardAvoidingView>
-      </SafeAreaView>
-    </ImageBackground>
+              <CustomButton
+                style={{ marginVertical: 8 }}
+                title="Submit"
+                onPress={handleLogin}
+                loading={loading}
+              />
+              <CustomButton
+                style={{ marginVertical: 8 }}
+                title="Register"
+                onPress={handleBack}
+              />
+            </>
+          ) : (
+            <>
+              <CustomTextInput
+                style={{ marginVertical: 8 }}
+                value={email}
+                onChangeText={setEmail}
+                placeholder="Enter your email"
+                textContentType="emailAddress"
+              />
+              <CustomTextInput
+                style={{ marginVertical: 8 }}
+                value={fullName}
+                onChangeText={setFullName}
+                placeholder="Enter your full name"
+              />
+              <CustomTextInput
+                style={{ marginVertical: 8 }}
+                value={mobile}
+                onChangeText={setMobile}
+                textContentType="telephoneNumber"
+                placeholder="Enter your phone number"
+              />
+              <CustomTextInput
+                style={{ marginVertical: 8 }}
+                value={password}
+                onChangeText={setPassword}
+                placeholder="Enter your password"
+                textContentType="password"
+              />
+              <CustomButton
+                style={{ marginVertical: 8 }}
+                title="Submit"
+                onPress={handleRegister}
+                loading={loading}
+              />
+              <CustomButton
+                style={{ marginVertical: 8 }}
+                title="Back"
+                onPress={handleBack}
+              />
+            </>
+          )}
+        </View>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 };
 
@@ -141,5 +150,14 @@ const styles = StyleSheet.create({
   image: {
     flex: 1,
     resizeMode: "stretch",
+  },
+  backgroundVideo: {
+    height: height,
+    position: "absolute",
+    top: 0,
+    left: 0,
+    alignItems: "stretch",
+    bottom: 0,
+    right: 0,
   },
 });
